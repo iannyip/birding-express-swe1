c0cb44e70f800e91e0ce9f701e44c914bd760530
iannyip <ianyip@nullspace.com.sg>
2021-02-23 00:18:44 +0800

    includes comments

--- create_tables.sql
+++ create_tables.sql
@@ -3,12 +3,51 @@
 
 DROP TABLE IF EXISTS notes;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS species;
+DROP TABLE IF EXISTS behaviours;
+DROP TABLE IF EXISTS comments;
 
-CREATE TABLE notes (id SERIAL PRIMARY KEY, datetime TIMESTAMPTZ, behaviour TEXT, flock_size INTEGER, user_id INTEGER);
 CREATE TABLE users (id SERIAL PRIMARY KEY, email TEXT, password TEXT);
+CREATE TABLE species (id SERIAL PRIMARY KEY, name TEXT, scientific_name TEXT);
+CREATE TABLE behaviours (id SERIAL PRIMARY KEY, name TEXT);
+CREATE TABLE comments (id SERIAL PRIMARY KEY, note_id INTEGER, commenter_id INTEGER, comment TEXT);
+CREATE TABLE notes (id SERIAL PRIMARY KEY, datetime TIMESTAMPTZ, behaviour_id INTEGER, flock_size INTEGER, user_id INTEGER);
 
-INSERT INTO notes (datetime, behaviour, flock_size, user_id) VALUES ('2021-01-31 21:00:00', 'Tail bobs', 2, 1);
-INSERT INTO notes (datetime, behaviour, flock_size, user_id) VALUES ('2021-02-01 22:00:00', 'Head cocks', 3, 2);
+
+INSERT INTO notes (datetime, behaviour_id, flock_size, user_id) VALUES ('2021-01-31 21:00:00', 2, 2, 1);
+INSERT INTO notes (datetime, behaviour_id, flock_size, user_id) VALUES ('2021-02-01 22:00:00', 5, 3, 2);
+INSERT INTO notes (datetime, behaviour_id, flock_size, user_id) VALUES ('2021-02-03 09:00:00', 1, 5, 2);
+INSERT INTO notes (datetime, behaviour_id, flock_size, user_id) VALUES ('2021-02-04 15:00:00', 2, 1, 2);
+INSERT INTO notes (datetime, behaviour_id, flock_size, user_id) VALUES ('2021-02-08 19:00:00', 6, 7, 2);
 
 INSERT INTO users (email, password) VALUES ('ian@gmail.com', 'ianpassword');
 INSERT INTO users (email, password) VALUES ('kai@gmail.com', 'kaipassword');
+
+INSERT INTO species (name, scientific_name) VALUES ('King Quail', 'Excalfactoria chinensis');
+INSERT INTO species (name, scientific_name) VALUES ('Red Junglefowl','Gallus gallus');
+INSERT INTO species (name, scientific_name) VALUES ('Wandering Whistling Duck', 'Dendrocygna arcuata');
+INSERT INTO species (name, scientific_name) VALUES ('Grey Nightjar', 'Caprimulgus jotaka');
+
+INSERT INTO behaviours (name) VALUES ('Walking');
+INSERT INTO behaviours (name) VALUES ('Resting');
+INSERT INTO behaviours (name) VALUES ('Gathering Nesting Materials');
+INSERT INTO behaviours (name) VALUES ('Mobbing');
+INSERT INTO behaviours (name) VALUES ('Long Song');
+INSERT INTO behaviours (name) VALUES ('Bathing');
+INSERT INTO behaviours (name) VALUES ('Preening');
+INSERT INTO behaviours (name) VALUES ('Territory Defence');
+INSERT INTO behaviours (name) VALUES ('Climbing Tree');
+INSERT INTO behaviours (name) VALUES ('Bark Feeding');
+INSERT INTO behaviours (name) VALUES ('Hunting');
+INSERT INTO behaviours (name) VALUES ('Flying');
+INSERT INTO behaviours (name) VALUES ('Tail bobs');
+INSERT INTO behaviours (name) VALUES ('Head cocks');
+
+INSERT INTO comments (note_id, commenter_id, comment) VALUES (2, 1, 'this is very good!');
+INSERT INTO comments (note_id, commenter_id, comment) VALUES (2, 2, 'nice!');
+INSERT INTO comments (note_id, commenter_id, comment) VALUES (3, 1, 'suj!');
+INSERT INTO comments (note_id, commenter_id, comment) VALUES (3, 2, 'bravo!');
+INSERT INTO comments (note_id, commenter_id, comment) VALUES (4, 2, 'got the bird wrong!');
+INSERT INTO comments (note_id, commenter_id, comment) VALUES (4, 1, 'this is fake');
+INSERT INTO comments (note_id, commenter_id, comment) VALUES (5, 1, 'nice!');
+INSERT INTO comments (note_id, commenter_id, comment) VALUES (5, 1, 'love it!');
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Step 1: Import modules
-import express from "express";
+import express, { query } from "express";
 import pg from "pg";
 import methodOverride from "method-override";
 import cookieParser from "cookie-parser";
@@ -43,7 +43,26 @@ const checkLoginCookie = (reqObj, resObj) => {
 // Step 3: Create routes
 app.get("/note", (request, response) => {
   console.log("GET note");
-  response.render("note");
+  // Verify login
+  if (checkLoginCookie(request, response)) {
+    return;
+  }
+  // Get the ID of the user
+  const { userId }= request.cookies;
+  console.log(userId);
+  pool.query('SELECT * FROM behaviours', (error, result) => {
+    if (error) {
+      console.log("error", error);
+    } else{
+      const allBehaviourArr = result.rows;
+      console.log(allBehaviourArr);
+      const noteData = {
+        behaviours: allBehaviourArr,
+        id: userId,
+      }
+      response.render("note", noteData);
+    }
+  });
 });
 
 app.post("/note", (request, response) => {
@@ -52,14 +71,14 @@ app.post("/note", (request, response) => {
   const submittedData = request.body;
   const inputData = [
     joinDateTime(submittedData.date, submittedData.time),
-    submittedData.behaviour,
+    submittedData.behaviour_id,
     submittedData.flock_size,
+    submittedData.user_id,
   ];
   console.log(inputData);
-
   // Query: Input into TABLE notes
   const querySubmitSighting =
-    "INSERT INTO notes (datetime, behaviour, flock_size) VALUES ($1, $2, $3)";
+    "INSERT INTO notes (datetime, behaviour_id, flock_size, user_id) VALUES ($1, $2, $3, $4)";
   pool.query(querySubmitSighting, inputData, (error, result) => {
     if (error) {
       console.log("submit error", error);
@@ -71,17 +90,33 @@ app.post("/note", (request, response) => {
 });
 
 app.get("/note/:id", (request, response) => {
+  // Verify login
+  if (checkLoginCookie(request, response)) {
+    return;
+  }
   const id = request.params.id;
   console.log(`GET note ${id}`);
+  // Query: Get note
   const querySingleSighting = `SELECT * FROM notes WHERE id = ${id}`;
   pool.query(querySingleSighting, (error, result) => {
     if (error) {
       console.log("single note error: ", error);
     } else {
       const queryResult = result.rows[0];
-      console.table(queryResult);
-      console.log(queryResult);
-      response.render("note_single", { noteObj: queryResult });
+      console.log(queryResult); // This is an object
+      const noteId = queryResult.id;
+      // Query: Get comments
+      const queryComments = (`SELECT comments.id, comments.comment, users.email FROM comments INNER JOIN users ON comments.commenter_id = users.id WHERE note_id = ${noteId} ORDER BY comments.id DESC`);
+      pool.query(queryComments, (errorComment, resultComment) => {
+        if (errorComment) {
+          console.log("error: ", errorComment);
+        } else {
+          queryResult.comments = resultComment.rows; // This is an array;
+          queryResult.commenter = request.cookies.userId;
+          console.log(queryResult);
+          response.render("note_single", { noteObj: queryResult });
+        }
+      })
     }
   });
 });
@@ -96,7 +131,7 @@ app.get("/", (request, response) => {
   }
 
   // Query: Get all information from TABLE notes
-  const queryRoot = "SELECT * FROM notes";
+  const queryRoot = "SELECT * FROM notes INNER JOIN behaviours ON notes.behaviour_id = behaviours.id";
   pool.query(queryRoot, (error, result) => {
     if (error) {
       console.log("root error: ", error.stack);
@@ -115,6 +150,10 @@ app.get("/", (request, response) => {
 });
 
 app.get("/note/:id/edit", (request, response) => {
+  // Verify login
+  if (checkLoginCookie(request, response)) {
+    return;
+  }
   console.log("GET NOTE EDIT");
   const noteId = request.params.id;
   // Query that particular id
@@ -145,7 +184,7 @@ app.put("/note/:id/edit", (request, response) => {
   ];
 
   // Query: update database
-  const queryUpdateNote = `UPDATE notes SET datetime=$1, behaviour=$2, flock_size=$3 WHERE id = ${noteId}`;
+  const queryUpdateNote = `UPDATE notes SET datetime=$1, behaviour_id=$2, flock_size=$3 WHERE id = ${noteId}`;
   pool.query(queryUpdateNote, inputData, (error, result) => {
     if (error) {
       console.log("error: ", error);
@@ -211,7 +250,7 @@ app.post("/login", (request, response) => {
       return;
     }
     if (result.rows.length === 0) {
-      response.status(403).send("sorry!");
+      response.status(403).send("sorry! Please try again!");
       return;
     }
     if (userInput.password === result.rows[0].password) {
@@ -229,6 +268,201 @@ app.delete("/logout", (request, response) => {
   response.clearCookie("userId");
   response.redirect("/login");
 });
+
+app.get('/species', (request, response) => {
+  console.log("GET species");
+  response.render('species');
+})
+
+app.post('/species', (request, response) =>{
+  console.log("POST species");
+  const newSpeciesData = request.body;
+  console.log(newSpeciesData);
+
+  // Query: Insert new data
+  const queryNewSpecies = "INSERT INTO species (name, scientific_name) VALUES ($1, $2)";
+  pool.query(queryNewSpecies, [newSpeciesData.name, newSpeciesData.scientific_name], (error, result) => {
+    if (error) {
+      console.log("error: ", error);
+    } else {
+      response.redirect('/');
+    }
+  })
+})
+
+app.get('/species/all', (request, response) => {
+  console.log("GET all species");
+
+  // Query to get all species
+  const queryAllSpecies = "SELECT * FROM species";
+  pool.query(queryAllSpecies, (error, result) => {
+    if (error){
+      console.log("error: ", error);
+    } else {
+      const allSpeciesData = result.rows;
+      response.render('species_all', {allSpeciesData});
+    }
+  })
+})
+
+app.get('/species/:index', (request, response) => {
+  console.log("GET SPECIES INDEX");
+  const {index} = request.params;
+  
+  // Query: particular species index
+  const querySpeciesIndex = `SELECT * FROM species WHERE id='${index}'`;
+  pool.query(querySpeciesIndex, (error, result) =>{
+    if (error){
+      console.log("error: ", error);
+    } else{
+      const speciesData = result.rows[0];
+      response.render('species_single', {speciesData});
+    }
+  })
+})
+
+app.get('/species/:index/edit', (request, response) => {
+  console.log("GET edit SPECIES");
+  const {index} = request.params;
+
+  // Query: get species info
+  const querySpeciesIndex = `SELECT * FROM species WHERE id='${index}'`;
+  pool.query(querySpeciesIndex, (error, result) =>{
+    if (error){
+      console.log("error: ", error);
+    } else{
+      const speciesData = result.rows[0];
+      response.render('species_single_edit', {speciesData});
+    }
+  })
+})
+
+app.put('/species/:index/edit', (request, response) => {
+  console.log("PUT species index");
+  const {index} = request.params;
+  const updatedData = request.body;
+  console.log(updatedData);
+
+  // Query: update species table
+  const queryUpdateSpecies = `UPDATE species SET name=$1, scientific_name=$2 WHERE id=${index}`;
+  pool.query(queryUpdateSpecies, [updatedData.name, updatedData.scientific_name], (error, result) =>{
+    if (error){
+      console.log("error: ", error);
+    } else {
+      console.log('updated');
+      response.redirect(`/species/${index}`)
+    }
+  } )
+})
+
+app.delete('/species/:index/delete', (request, response) => {
+  console.log("DELETE species index");
+  const {index} = request.params;
+
+  // Query: Delete 
+  const queryDeleteSpecies = `DELETE FROM species WHERE id=${index}`;
+  pool.query(queryDeleteSpecies, (error, result) => {
+    if (error) {
+      console.log('error: ', error);
+    } else{
+      response.redirect('/species/all');
+    }
+  })
+})
+
+app.get('/behaviours', (request, response) => {
+  console.log("GET behaviours");
+
+  // Query: Retrieve all behaviours
+  const queryBehaviour = "SELECT * FROM behaviours";
+  pool.query(queryBehaviour, (error, result) => {
+    if (error){
+      console.log("error: ", error);
+    } else {
+      const allBehaviourArr = result.rows;
+      let counter = 0;
+      allBehaviourArr.forEach(behaviour => {
+        pool.query(`SELECT COUNT(*) FROM notes WHERE behaviour_id='${behaviour.id}'`, (errorNoteCount, resultNoteCount) => {
+          counter += 1;
+          if (errorNoteCount){
+            console.log("error", errorNoteCount);
+          } else{
+            behaviour.count = Number(resultNoteCount.rows[0].count);
+          }
+
+          if (counter === allBehaviourArr.length){
+            response.render('behaviours', {allBehaviourArr});
+          }
+
+        });
+      })
+      
+    }
+  })
+})
+
+app.get('/behaviours/:id', (request, response) => {
+  console.log("GET BEHAVIOUR ID");
+  const {id} = request.params;
+
+  // Query
+  const queryBehaviourId = `SELECT * FROM notes WHERE behaviour_id = ${id}`;
+  pool.query(queryBehaviourId, (error, result) => {
+    if (error) {
+      console.log("error: ", error);
+    } else {
+      const notesData = result.rows;
+      console.log({notesData});
+      notesData.forEach((note) => {
+        note.date = separateDateTime(note.datetime)[0];
+        note.time = separateDateTime(note.datetime)[1];
+      });
+      response.render('behaviours_single', {notesData});
+    }
+  })
+})
+
+app.post('/note/:id/comment', (request, response) =>{
+  console.log("POST Comment");
+  const newComment = request.body;
+  console.log(newComment);
+  pool.query(`INSERT INTO comments (note_id, commenter_id, comment) VALUES (${newComment.note_id}, ${newComment.commenter_id}, '${newComment.comment}')`, (error, result) => {
+    if (error) {
+      console.log("error: ", error);
+    } else {
+      response.redirect(`/note/${newComment.note_id}`);
+    }
+  })
+})
+
+app.get('/user', (request, response) => {
+  console.log("GET user");
+    // Verify login
+  if (checkLoginCookie(request, response)) {
+    return;
+  }
+  const {userId} = request.cookies;
+  const userActivity = {};
+  const queryUserActivity = `SELECT * FROM notes WHERE id=${userId}`;
+  pool.query(queryUserActivity, (noteError, noteResult) => {
+    if (noteError){
+      console.log("error: ", noteError);
+    } else{
+      userActivity.notes = noteResult.rows;
+      console.log(noteResult.rows);
+      pool.query(`SELECT * FROM comments WHERE commenter_id=${userId}`, (commentError, commentResult) => {
+        if (commentError){
+          console.log("error: ", commentError);
+        } else {
+          userActivity.comments = commentResult.rows;
+          console.log("the user has: ", userActivity);
+        }
+      })
+    }
+  })
+
+})
+
 
 // Step 4: Start Server
 console.log("starting server...");
